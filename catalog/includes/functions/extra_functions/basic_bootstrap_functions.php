bf22c23a0d3378b3097b9383050bddbc0e8e982e
lingcarzy <lingcarzy@gmail.com>
2018-04-13 09:15:20 +0800

    更正注释中的路径

--- catalog/includes/functions/extra_functions/basic_bootstrap_functions.php
+++ catalog/includes/functions/extra_functions/basic_bootstrap_functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author      lingcarzy <lingcarzy@gmail.com>
- * @license     https://github.com/lingcarzy/zencart-bootstrap-theme/blob/master/LICENSE
+ * @license     https://github.com/lingcarzy/basic-bootstrap-for-zencart/blob/master/LICENSE
  *
  * ZC BootStrap Template Default
  */
