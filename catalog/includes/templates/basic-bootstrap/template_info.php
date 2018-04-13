31003550dd597814086f4c6a3fb291fdbde61555
lingcarzy <lingcarzy@gmail.com>
2018-04-13 09:10:58 +0800

    更名为basic-bootstrap

--- catalog/includes/templates/basic-bootstrap/template_info.php
+++ catalog/includes/templates/basic-bootstrap/template_info.php
@@ -6,11 +6,11 @@
  * @copyright Copyright 2003-2005 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: template_info.php 2306 2005-11-05 21:34:28Z wilt $
+ * @version $Id: template_info.php 2306 2018-04-13 09:10:28Z wilt $
  */
-$template_name = 'BootStrap Template';
+$template_name = 'Basic BootStrap Template';
 $template_version = 'Version 1.0';
 $template_author = 'Lingcarzy@gmail.com 2017';
-$template_description = 'BootStrap basic theme';
+$template_description = 'BootStrap basic theme for Zen-cart';
 $template_screenshot = '';
 ?>
