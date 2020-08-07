681a8d3dede0407ae7f1627462bfd5841c10b227
lingcarzy <lingcarzy@gmail.com>
2020-08-07 16:07:20 +0800

    Update basic_bootstrap_functions.php

--- catalog/includes/functions/extra_functions/basic_bootstrap_functions.php
+++ catalog/includes/functions/extra_functions/basic_bootstrap_functions.php
@@ -5,3 +5,15 @@
  *
  * ZC BootStrap Template Default
  */
+function basic_element($name = 'div',$parm = array(),$val = ""){
+	$tag = "<".$name." ";
+	foreach ($parm as $key => $value){
+		$tag .= $key.'="'.$value.'" ';
+	}
+	if(empty($val)) {
+		$tag.='/>';
+	}	else {
+		$tag.='>'.$val.'</'.$name.'>';
+	}
+	return $tag;
+}
